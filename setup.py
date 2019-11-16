d053d0c8685ab3142803373afbb339abb6b98abd
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-11-16 18:18:40 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python_snippets',
+    version='0.0.1',
+    description='一些常用的python代码合集',
+    license='MIT Licence',
+    url='https://kexue.fm',
+    author='bojone',
+    author_email='bojone@spaces.ac.cn',
+    install_requires=[],
+    packages=find_packages()
+)
