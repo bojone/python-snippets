b72ffec4d0a6e61016cb1a788391f98bad9f3b3d
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-11-16 18:21:06 +0800

    Update snippets.py

--- python_snippets/snippets.py
+++ python_snippets/snippets.py
@@ -30,3 +30,70 @@ class Progress:
             if (i + 1) % self.period == 0:
                 self.logger.info(self._format_ % (i + 1))
             yield j
+
+
+def parallel_apply(func,
+                   iterable,
+                   workers,
+                   max_queue_size,
+                   callback=None,
+                   dummy=False):
+    """多进程或多线程地将func应用到iterable的每个元素中。
+    注意这个apply是异步且无序的，也就是说依次输入a,b,c，但是
+    输出可能是func(c), func(a), func(b)。
+    参数：
+        dummy: False是多进程/线性，True则是多线程/线性；
+        callback: 处理单个输出的回调函数；
+    """
+    if dummy:
+        from multiprocessing.dummy import Pool, Queue
+    else:
+        from multiprocessing import Pool, Queue
+
+    in_queue, out_queue = Queue(max_queue_size), Queue()
+
+    def worker_step(in_queue, out_queue):
+        # 单步函数包装成循环执行
+        while True:
+            d = in_queue.get()
+            r = func(d)
+            out_queue.put(r)
+
+    # 启动多进程/线程
+    pool = Pool(workers, worker_step, (in_queue, out_queue))
+
+    if callback is None:
+        results = []
+
+    # 后处理函数
+    def process_out_queue():
+        out_count = 0
+        for _ in range(out_queue.qsize()):
+            d = out_queue.get()
+            out_count += 1
+            if callback is None:
+                results.append(d)
+            else:
+                callback(d)
+        return out_count
+
+    # 存入数据，取出结果
+    in_count, out_count = 0, 0
+    for d in iterable:
+        in_count += 1
+        while True:
+            try:
+                in_queue.put(d, block=False)
+                break
+            except six.moves.queue.Full:
+                out_count += process_out_queue()
+        if in_count % max_queue_size == 0:
+            out_count += process_out_queue()
+
+    while out_count != in_count:
+        out_count += process_out_queue()
+
+    pool.terminate()
+
+    if callback is None:
+        return results
